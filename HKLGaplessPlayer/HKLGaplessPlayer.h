b25656b832e54a25a13299cac2f4254a53ff0fb7
Hirohito Kato <hkato193@gmail.com>
2015-01-06 18:59:59 +0900

    Add HKLGaplessPlayer framework target.

---
+++ HKLGaplessPlayer/HKLGaplessPlayer.h
@@ -0,0 +1,19 @@
+//
+//  HKLGaplessPlayer.h
+//  HKLGaplessPlayer
+//
+//  Created by Hirohito Kato on 2015/01/06.
+//  Copyright (c) 2015年 Hirohito Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HKLGaplessPlayer.
+FOUNDATION_EXPORT double HKLGaplessPlayerVersionNumber;
+
+//! Project version string for HKLGaplessPlayer.
+FOUNDATION_EXPORT const unsigned char HKLGaplessPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HKLGaplessPlayer/PublicHeader.h>
+#import <HKLGaplessPlayer/HKLGLPixelBufferView.h>
+
