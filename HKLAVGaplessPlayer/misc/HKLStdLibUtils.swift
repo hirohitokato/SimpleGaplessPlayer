127acc8b1a2291f315d771dc6ee81e36417ce053
Hirohito Kato <hkato193@gmail.com>
2017-10-19 12:37:59 +0900

    `Array` now has `index(of:)` method.

--- HKLAVGaplessPlayer/misc/HKLStdLibUtils.swift
+++ HKLAVGaplessPlayer/misc/HKLStdLibUtils.swift
@@ -8,27 +8,27 @@
 import Foundation
 
 
-extension Array {
-    /**
-    Returns the lowest index whose corresponding array value matches a given condition.
-
-    :discussion: Starting at index 0, each element of the array is passed as the 1st parameter of condition closure until a match is found or the end of the array is reached. Objects are considered equal if the closure returns true.
-    
-    :param: condition A closure for finding the target object in the array
-
-    :returns: The lowest index whose corresponding array value matches a given condition. If none of the objects in the array matches the condition, returns nil.
-
-    :refer: http://stackoverflow.com/a/24105493
-    */
-    func indexOf(@noescape condition: Array.Element -> Bool) -> Array.Index? {
-        for idx in indices(self) {
-            if condition(self[idx]) {
-                return idx
-            }
-        }
-        return nil
-    }
-}
+//extension Array {
+//    /**
+//    Returns the lowest index whose corresponding array value matches a given condition.
+//
+//    :discussion: Starting at index 0, each element of the array is passed as the 1st parameter of condition closure until a match is found or the end of the array is reached. Objects are considered equal if the closure returns true.
+//    
+//    :param: condition A closure for finding the target object in the array
+//
+//    :returns: The lowest index whose corresponding array value matches a given condition. If none of the objects in the array matches the condition, returns nil.
+//
+//    :refer: http://stackoverflow.com/a/24105493
+//    */
+//    func indexOf(condition: (Array.Element) -> Bool) -> Array.Index? {
+//        for idx in indices(self) {
+//            if condition(self[idx]) {
+//                return idx
+//            }
+//        }
+//        return nil
+//    }
+//}
 
 /**
 Returns the first index where condition matches in domain or nil if it does not matched.
